4cf58dec6b8e4e3292491321db1d284994ae0e54
Ross Fairbanks <ross@rossfairbanks.com>
2015-01-01 11:18:16 +0100

    Add create_segment to CrawlSegment

--- lib/elasticrawl/crawl_segment.rb
+++ lib/elasticrawl/crawl_segment.rb
@@ -4,5 +4,29 @@ module Elasticrawl
   class CrawlSegment < ActiveRecord::Base
     belongs_to :crawl
     has_many :job_steps
+
+    # Creates a crawl segment based on its S3 path if it does not exist.
+    def self.create_segment(crawl, segment_name, file_count)
+      s3_uri = build_s3_uri(crawl.crawl_name, segment_name)
+
+      segment = CrawlSegment.where(:crawl_id => crawl.id,
+                                  :segment_name => segment_name,
+                                  :segment_s3_uri => s3_uri,
+                                  :file_count => file_count).first_or_create
+    end
+
+private
+    # Generates the S3 location where this segment is stored.
+    def self.build_s3_uri(crawl_name, segment_name)
+      s3_path = [ Elasticrawl::COMMON_CRAWL_PATH,
+                  crawl_name,
+                  segment_name,
+                  Elasticrawl::SEGMENTS_PATH,
+                  '' ]
+
+      URI::Generic.build(:scheme => 's3',
+                         :host => Elasticrawl::COMMON_CRAWL_BUCKET,
+                         :path => s3_path.join('/').to_s)
+    end
   end
 end
--- spec/unit/crawl_segment_spec.rb
+++ spec/unit/crawl_segment_spec.rb
@@ -8,21 +8,24 @@ describe Elasticrawl::CrawlSegment do
   it { should have_db_column(:parse_time).of_type(:datetime) }
   it { should have_db_column(:file_count).of_type(:integer) }
 
-  describe '#initialize' do
-    let(:crawl) { Elasticrawl::Crawl.create(:crawl_name => 'CC-MAIN-2013-20') }
-    subject { crawl.crawl_segments[0]}
-
-    before do
-      crawl.create_segments
-    end
-
+  describe '.create_segment' do
+    let(:crawl) { Elasticrawl::Crawl.create(:crawl_name => 'CC-MAIN-2014-49') }
+    let(:segment_name) { '1416400372202.67' }
+    let(:file_count) { 3 }
+    subject { Elasticrawl::CrawlSegment.create_segment(crawl,
+                                                       segment_name,
+                                                       file_count) }
     it 'should have a segment name' do
-      expect(subject.segment_name).to eq '1368696381249'
+      expect(subject.segment_name).to eq segment_name
     end
 
     it 'should have an s3 uri' do
       expect(subject.segment_s3_uri).to eq \
-        's3://aws-publicdatasets/common-crawl/crawl-data/CC-MAIN-2013-20/segments/1368696381249/'
+        "s3://aws-publicdatasets/common-crawl/crawl-data/#{crawl.crawl_name}/#{segment_name}/segments/"
+    end
+
+    it 'should have a file count' do
+      expect(subject.file_count).to eq file_count
     end
   end
 end
